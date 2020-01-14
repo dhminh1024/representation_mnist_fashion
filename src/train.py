46ba80d3d0874de6cb776a99c98b610e11cea8e0
minhdh <dhminh1024@gmail.com>
2020-01-14 15:40:45 +0700

    fix bugs 1

--- src/train.py
+++ src/train.py
@@ -4,12 +4,20 @@ import generator
 import config as c
 from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
 
+import datetime
+import argparse
+import os
+import pathlib
+import sys
+
+CHECKPOINT_PATH = os.path.join('.', 'evaluation', 'checkpoint_weights.hdf5')
+
 def train_model(train_ds, test_ds, learning_rate=0.001):
     model = mb.build_model(learning_rate=learning_rate)
 
     callbacks = [
         ModelCheckpoint(
-            filepath='checkpoint_weights.hdf5',
+            filepath=CHECKPOINT_PATH,
             monitor='val_loss',
             save_best_only=True,
             save_weights_only=True,
@@ -28,14 +36,14 @@ def train_model(train_ds, test_ds, learning_rate=0.001):
             verbose=1)
     ]
 
-    model.fit(train_ds,
+    history = model.fit(train_ds,
            epochs=c.NUM_EPOCHS,
            steps_per_epoch=int(c.NUM_TRAIN_EXAMPLES/c.BATCH_SIZE),
            validation_data=test_ds, 
            validation_steps=int(c.NUM_TEST_EXAMPLES/c.BATCH_SIZE),
            callbacks=callbacks)
 
-    return model
+    return model, history
 
 
 if __name__ == "__main__":
@@ -47,4 +55,29 @@ if __name__ == "__main__":
     if args.train:
         train_ds, test_ds, ds_info = generator.build_dataset()
 
-        model = train_model(train_ds, test_ds)
+        start_time = datetime.datetime.now()
+
+        model, h = train_model(train_ds, test_ds)
+
+        total_time = datetime.datetime.now() - start_time
+
+        loss = h.history['loss']
+        val_loss = h.history['val_loss']
+
+        min_val_loss = min(val_loss)
+        min_val_loss_i = val_loss.index(min_val_loss)
+
+        time_epoch = (total_time / len(loss))
+
+        t_corpus = "\n".join([
+            "Batch:                   {}\n".format(BATCH_SIZE),
+            "Time per epoch:          {}".format(time_epoch),
+            "Total epochs:            {}".format(len(loss)),
+            "Best epoch               {}\n".format(min_val_loss_i + 1),
+            "Training loss:           {}".format(loss[min_val_loss_i]),
+            "Validation loss:         {}".format(min_val_loss),
+        ])
+
+        with open(os.path.join('.', 'evaluation', 'train.txt'), "w") as f:
+            f.write(t_corpus)
+            print(t_corpus)
